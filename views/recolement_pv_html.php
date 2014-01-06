3f77b91377c204764bd67f896f7bed371f3e81e6
gautiermichelin <gm@ideesculture.com>
2014-01-06 10:56:07 +0100

    code reformating ; typo

--- views/recolement_pv_html.php
+++ views/recolement_pv_html.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 $InfosPv = $this->getVar('InfosPv');
 ?>
@@ -7,7 +7,7 @@ $InfosPv = $this->getVar('InfosPv');
 
 <h2>
 	<a href="<?php print __CA_URL_ROOT__; ?>/index.php/editor/occurrences/OccurrenceEditor/Summary/occurrence_id/<?php print $InfosPv["info"]["occurrence_id"]; ?>">
-	<img src="<?php print __CA_URL_ROOT__; ?>/themes/default/graphics/buttons/edit.png"></a>
+		<img src="<?php print __CA_URL_ROOT__; ?>/themes/default/graphics/buttons/edit.png"></a>
 	<?php print $InfosPv["info"]["campagne_nom"] ?>
 </h2>
 
@@ -20,15 +20,18 @@ $InfosPv = $this->getVar('InfosPv');
 		border-radius: 5px;
 		background-color: #d4d4d4;
 	}
+
 	progress::-webkit-progress-bar {
 		border-radius: 5px;
 		background-color: #d4d4d4;
 	}
+
 	progress::-webkit-progress-value {
 		border-radius: 5px;
 		background-color: #1ab4c8;
 		background-size: 40px 40px;
 	}
+
 	progress::-moz-progress-bar {
 		border-radius: 5px;
 		background-color: #1ab4c8;
@@ -39,28 +42,93 @@ $InfosPv = $this->getVar('InfosPv');
 </style>
 
 <table class="listtable">
-	<tr><td>Numéro de campagne</td><td><?php print $InfosPv["info"]["idno"]; ?></td></tr>
-	<tr class="odd"><td>Date de campagne</td><td><?php print $InfosPv["info"]["campagne_date"]; ?></td></tr>
-	<tr><td>Caractéristiques</td><td><?php print $InfosPv["info"]["campagne_caracteristiques"]; ?></td></tr>
-	<tr class="odd"><td>Moyens</td><td><?php print $InfosPv["info"]["campagne_moyens"]; ?></td></tr>
-	<tr><td>Champs couverts</td><td><?php print $InfosPv["info"]["campagne_champs_champs"]; ?></td></tr>
-	<tr><td>Contenu scientifique de la campagne</td><td><?php print $InfosPv["info"]["contenu_scientifique"]; ?></td></tr>
-	<tr class="odd"><td><small>Note</small></td><td><small><?php print $InfosPv["info"]["campagne_champs_note"]; ?></small></td></tr>
-	<tr><td>Avancement</td><td><progress id="avancement" value="<?php print (int) $InfosPv["info"]["recolements_done"]; ?>" max="<?php print $InfosPv["info"]["recolements_total"]; ?>"></progress></td></tr>
-	<tr class="odd"><td>Objets vus</td><td><?php print (int)$InfosPv["nb"]["objets_vus"]; ?></td></tr>
-	<tr><td>Objets manquants</td><td><?php print (int)$InfosPv["nb"]["objets_manquants"]; ?></td></tr>
-	<tr class="odd"><td>Objets non vus</td><td><?php print (int)$InfosPv["nb"]["objets_non_vus"]; ?></td></tr>
-	<tr><td>Objets détruits</td><td><?php print (int)$InfosPv["nb"]["objets_detruits"]; ?></td></tr>
-	<tr class="odd"><td>Objets non inventoriés</td><td><?php print (int)$InfosPv["nb"]["objets_non_inventories"]; ?></td></tr>
-	<tr><td>Objets inventoriés plusieurs fois</td><td><?php print (int)$InfosPv["nb"]["objets_inventories_plusieurs_fois"]; ?></td></tr>
-	<tr class="odd"><td>Objets marqués</td><td><?php print (int)$InfosPv["nb"]["objets_marques"]; ?></td></tr>
-	<tr><td>Objets non marqués</td><td><?php print (int)$InfosPv["nb"]["objets_non_marques"]; ?></td></tr>
-	<tr class="odd"><td>Objets exposés</td><td><?php print (int)$InfosPv["nb"]["objets_exposes"]; ?></td></tr>
-	<tr><td>Objets en réserve</td><td><?php print (int)$InfosPv["nb"]["objets_en_reserve"];?></td></tr>
-	<tr class="odd"><td>Total objets récolés</td><td><?php print (int)$InfosPv["nb"]["objets_recoles"]; ?></td></tr>
+	<tr>
+		<td>Numéro de campagne</td>
+		<td><?php print $InfosPv["info"]["idno"]; ?></td>
+	</tr>
+	<tr class="odd">
+		<td>Date de campagne</td>
+		<td><?php print $InfosPv["info"]["campagne_date"]; ?></td>
+	</tr>
+	<tr>
+		<td>Caractéristiques</td>
+		<td><?php print $InfosPv["info"]["campagne_caracteristiques"]; ?></td>
+	</tr>
+	<tr class="odd">
+		<td>Moyens</td>
+		<td><?php print $InfosPv["info"]["campagne_moyens"]; ?></td>
+	</tr>
+	<tr>
+		<td>Champs couverts</td>
+		<td><?php print $InfosPv["info"]["campagne_champs_champs"]; ?></td>
+	</tr>
+	<tr>
+		<td>Contenu scientifique de la campagne</td>
+		<td><?php print $InfosPv["info"]["contenu_scientifique"]; ?></td>
+	</tr>
+	<tr class="odd">
+		<td>
+			<small>Note</small>
+		</td>
+		<td>
+			<small><?php print $InfosPv["info"]["campagne_champs_note"]; ?></small>
+		</td>
+	</tr>
+	<tr>
+		<td>Avancement</td>
+		<td>
+			<progress id="avancement" value="<?php print (int)$InfosPv["info"]["recolements_done"]; ?>"
+			          max="<?php print $InfosPv["info"]["recolements_total"]; ?>"></progress>
+		</td>
+	</tr>
+	<tr class="odd">
+		<td>Objets vus</td>
+		<td><?php print (int)$InfosPv["nb"]["objets_vus"]; ?></td>
+	</tr>
+	<tr>
+		<td>Objets manquants</td>
+		<td><?php print (int)$InfosPv["nb"]["objets_manquants"]; ?></td>
+	</tr>
+	<tr class="odd">
+		<td>Objets non vus</td>
+		<td><?php print (int)$InfosPv["nb"]["objets_non_vus"]; ?></td>
+	</tr>
+	<tr>
+		<td>Objets détruits</td>
+		<td><?php print (int)$InfosPv["nb"]["objets_detruits"]; ?></td>
+	</tr>
+	<tr class="odd">
+		<td>Objets non inventoriés</td>
+		<td><?php print (int)$InfosPv["nb"]["objets_non_inventories"]; ?></td>
+	</tr>
+	<tr>
+		<td>Objets inventoriés plusieurs fois</td>
+		<td><?php print (int)$InfosPv["nb"]["objets_inventories_plusieurs_fois"]; ?></td>
+	</tr>
+	<tr class="odd">
+		<td>Objets marqués</td>
+		<td><?php print (int)$InfosPv["nb"]["objets_marques"]; ?></td>
+	</tr>
+	<tr>
+		<td>Objets non marqués</td>
+		<td><?php print (int)$InfosPv["nb"]["objets_non_marques"]; ?></td>
+	</tr>
+	<tr class="odd">
+		<td>Objets exposés</td>
+		<td><?php print (int)$InfosPv["nb"]["objets_exposes"]; ?></td>
+	</tr>
+	<tr>
+		<td>Objets en réserve</td>
+		<td><?php print (int)$InfosPv["nb"]["objets_en_reserve"]; ?></td>
+	</tr>
+	<tr class="odd">
+		<td>Total objets récolés</td>
+		<td><?php print (int)$InfosPv["nb"]["objets_recoles"]; ?></td>
+	</tr>
 </table>
 
-<a class="form-button" href="<?php print __CA_URL_ROOT__; ?>/index.php/recolementSmf/Recolement/PvWord/?idno=<?php print $InfosPv["info"]["idno"]; ?>">
+<a class="form-button"
+   href="<?php print __CA_URL_ROOT__; ?>/index.php/museesDeFrance/Recolement/PvWord/?idno=<?php print $InfosPv["info"]["idno"]; ?>">
 	<span class="form-button">
 	Générer le procès-verbal
 	</span>
