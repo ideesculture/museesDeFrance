af40040d7cb0f93fcfdc91289bb3b9cc3d1280b5
gautiermichelin <gm@ideesculture.com>
2015-08-07 15:51:44 +0200

    expanding model classes

--- lib/inventaire/BienAffecte.php
+++ lib/inventaire/BienAffecte.php
@@ -9,5 +9,11 @@
 require_once("BaseObjectInventaire.php");
 
 class BienAffecte extends BaseObjectInventaire {
+    function __construct($num = null)
+    {
+        $this->tablename = "inventaire_inventaire";
+        $this->fields = array("numinv","designation","materiaux","techniques","mesures","etat","epoque","utilisation","provenance");
 
+        parent::__construct($num);
+    }
 }
--- lib/inventaire/BienDepose.php
+++ lib/inventaire/BienDepose.php
@@ -11,4 +11,13 @@ require_once("BaseObjectInventaire.php");
 class BienDepose extends BaseObjectInventaire {
     public $numdep;
 
+    function __construct($num = null)
+    {
+        $this->tablename = "inventaire_depot";
+        $this->fields = array("numdep","numinv","date_ref_acte_depot","date_entree", "proprietaire", "date_ref_acte_fin" ,
+            "date_inscription","designation","inscription","materiaux","techniques","mesures","etat","epoque",
+            "utilisation","provenance","observations");
+
+        parent::__construct($num);
+    }
 }
