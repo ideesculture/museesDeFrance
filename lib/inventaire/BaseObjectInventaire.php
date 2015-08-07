3f369291a093630f5fb55c18a467c1cbcf7a9523
gautiermichelin <gm@ideesculture.com>
2015-08-07 11:25:39 +0200

    implementing the interface inside the base class

--- lib/inventaire/BaseObjectInventaire.php
+++ lib/inventaire/BaseObjectInventaire.php
@@ -6,8 +6,41 @@
  * Time: 09:14
  */
 
-require_once("AbstractObjectInventaire.php");
+require_once("InterfaceInventaire.php");
 
-class BaseObjectInventaire implements AbstractObjectInventaire {
+class BaseObjectInventaire implements InterfaceInventaire {
 
+    function get() {
+        return true;
+    }
+
+    function set() {
+        return true;
+    }
+
+    function Validate() {
+        return true;
+    }
+
+    function Unvalidate() {
+        return true;
+    }
+
+    function getPhoto() {
+        return true;
+    }
+
+    function getTitle() {
+        return true;
+    }
+
+    function getInventaireNumber() {
+        return true;
+    }
+    function getHTML() {
+        return true;
+    }
+    function getJSON() {
+        return true;
+    }
 }
