1bd850a79551298a5f02042ef666b3b34f0ca6e8
gautiermichelin <gm@ideesculture.com>
2015-08-07 15:38:29 +0200

    transfert action updated

--- controllers/InventaireBiensAffectesController.php
+++ controllers/InventaireBiensAffectesController.php
@@ -22,14 +22,20 @@ class InventaireBiensAffectesController extends ActionController
 
     public function Transfert()
     {
-        $vs_object_id = $this->opo_request->getParameter("id",pInteger);
-        $vt_object = new ca_objects($vs_object_id);
-        $vo_bienaffecte = new BienAffecte($vt_object->get("idno"));
-        $vo_bienaffecte->set("designation",$vt_object->get("name"));
-        $vo_bienaffecte->save();
+        $vs_object_id = $this->request->getParameter("id",pInteger);
 
-        $this->view->setVar('idno', $vt_object->get("idno"));
-        $this->view->setVar('name', $vt_object->get("name"));
+        $vt_object = new ca_objects($vs_object_id);
+        $vs_idno = $vt_object->get("idno");
+        $vs_name = $vt_object->get("ca_objects.preferred_labels.name");
+
+        $vo_bienaffecte = new BienAffecte($vs_idno);
+        $vo_bienaffecte->set("designation",$vs_name);
+        $vo_bienaffecte->save();
+        //var_dump($vo_bienaffecte);
+        //die();
+
+        $this->view->setVar('idno', $vs_idno);
+        $this->view->setVar('name', $vs_name);
         $this->render('inventaire_biens_affectes_transfert_html.php');
     }
 
--- views/inventaire_biens_affectes_transfert_html.php
+++ views/inventaire_biens_affectes_transfert_html.php
@@ -4,8 +4,8 @@
     $vs_name = $this->getVar("name");
 
 ?>
-<h1>Transfert dans l'inventaire des biens affectés</h1>
-<p>Vous avez préparé les informations de l'objet <?php print $name; ?> pour les transférer dans le registre d'inventaire.</p>
+<h1><?php print $vs_name." [".$vs_idno."]"; ?> <br/><small>Transfert dans l'inventaire des biens affectés</small></h1>
+<p>Vous avez préparé les informations de l'objet <em><?php print $vs_name." [".$vs_idno."]"; ?></em> pour les transférer dans le registre d'inventaire.</p>
 <p>Attention, ces données ne seront définitivement écrites qu'à la validation de l'objet dans l'inventaire.</p>
 <p>Une fois l'objet validé, vous ne pourrez plus modifier ses informations.</p>
 <p>Voulez vous valider l'ajout de l'objet dans l'inventaire.</p>
