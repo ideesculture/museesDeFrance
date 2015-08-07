33e2dd5d0381025f3431df3837b0c4b33cc53f88
gautiermichelin <gm@ideesculture.com>
2015-08-07 15:27:52 +0200

    creating a better view

---
+++ views/inventaire_biens_affectes_transfert_html.php
@@ -0,0 +1,13 @@
+<?php
+    $vn_id = $this->getVar("id");
+    $vs_idno = $this->getVar("idno");
+    $vs_name = $this->getVar("name");
+
+?>
+<h1>Transfert dans l'inventaire des biens affectés</h1>
+<p>Vous avez préparé les informations de l'objet <?php print $name; ?> pour les transférer dans le registre d'inventaire.</p>
+<p>Attention, ces données ne seront définitivement écrites qu'à la validation de l'objet dans l'inventaire.</p>
+<p>Une fois l'objet validé, vous ne pourrez plus modifier ses informations.</p>
+<p>Voulez vous valider l'ajout de l'objet dans l'inventaire.</p>
+<?php print caNavButton($this->request, __CA_NAV_BUTTON_APPROVE__, "Valider", '', $this->request->getModulePath(), $this->request->getController(), 'Validate', array("id"=>$vn_id)); ?>
+
