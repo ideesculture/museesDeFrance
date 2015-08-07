f8998646d5c80990ed4fd974b031b5b3eaf71f52
csolievna <c.solievna@gmail.com>
2015-08-07 11:29:06 +0200

    Coucou vue

--- controllers/InventaireBiensAffectesController.php
+++ controllers/InventaireBiensAffectesController.php
@@ -1,7 +1,25 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: gautier
- * Date: 07/08/15
- * Time: 08:59
- */
+
+require_once(__CA_LIB_DIR__ . '/core/Configuration.php');
+require_once(__CA_MODELS_DIR__ . '/ca_objects.php');
+require_once(__CA_MODELS_DIR__ . '/ca_occurrences.php');
+require_once(__CA_LIB_DIR__ . '/ca/Search/OccurrenceSearch.php');
+require_once(__CA_LIB_DIR__ . '/ca/Search/SetSearch.php');
+
+
+class InventaireBiensAffectesController extends ActionController
+{
+    # -------------------------------------------------------
+    #
+    # -------------------------------------------------------
+    public function __construct(&$po_request, &$po_response, $pa_view_paths = null)
+    {
+        parent::__construct($po_request, $po_response, $pa_view_paths);
+    }
+
+    public function Index()
+    {
+        $this->render('coucou_html.php');
+    }
+
+}
