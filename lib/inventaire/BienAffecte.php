46767f32db545368b5b6cc26f5f241e56f64a300
gautiermichelin <gm@ideesculture.com>
2015-08-09 21:04:45 +0200

    missing validated field to allow object validation

--- lib/inventaire/BienAffecte.php
+++ lib/inventaire/BienAffecte.php
@@ -42,7 +42,7 @@ class BienAffecte extends BaseObjectInventaire {
     function __construct($num = null)
     {
         $this->tablename = "inventaire_inventaire";
-        $this->fields = array("ca_id", "numinv","numinv_sort","numinv_display", "designation", "designation_display", "mode_acquisition", "donateur", "date_acquisition", "avis", "prix", "date_inscription", "date_inscription_display", "observations", "inscription", "materiaux", "techniques", "mesures", "etat", "auteur", "auteur_display", "epoque", "utilisation","provenance");
+        $this->fields = array("ca_id", "numinv","numinv_sort","numinv_display", "designation", "designation_display", "mode_acquisition", "donateur", "date_acquisition", "avis", "prix", "date_inscription", "date_inscription_display", "observations", "inscription", "materiaux", "techniques", "mesures", "etat", "auteur", "auteur_display", "epoque", "utilisation","provenance","validated");
 
         $this->mapping = getMappingBiensAffectes();
 
