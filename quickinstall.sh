06de760f92f3e6a496c4c3c716c7e5e09fded01c
gautiermichelin <gm@ideesculture.com>
2016-02-15 17:58:16 +0100

    making symlink for profile

--- quickinstall.sh
+++ quickinstall.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
 # TODO HERE : script to make symlink with the profile allowing quick installation
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR
+ln -s assets/profile/joconde-sans-thesaurus-archives-documentation.xml ../../../install/profiles/xml/joconde.xml
