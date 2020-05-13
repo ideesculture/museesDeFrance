3c8d98f28750e2f4bd67d0cf66dd43c5662a51a8
Gautier Michelin <gautier.michelin@gmail.com>
2020-05-13 11:14:13 +0200

    icon

--- views/joconde_export_genere_html.php
+++ views/joconde_export_genere_html.php
@@ -1,6 +1,6 @@
 <h1>Export Joconde</h1>
 <p>L'export du panier Joconde a été réalisé.</p>
-<p><a href="<?php print __CA_URL_ROOT__; ?>/index.php/museesDeFrance/Joconde/Index" class="form-button 1487955007"><span class="form-button"><img src="/gestion/themes/default/graphics/buttons/glyphicons_224_chevron-left.png" border="0" class="form-button-left" style="padding-right: 10px">Retour</span></a></p>
+<p><a href="<?php print __CA_URL_ROOT__; ?>/index.php/museesDeFrance/Joconde/Index" class="form-button 1487955007"><span class="form-button"><?php print caNavIcon(__CA_NAV_ICON_GO__); ?>Retour</span></a></p>
 
 <hr/>
 <small>
