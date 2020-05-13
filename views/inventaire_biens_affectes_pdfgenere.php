d3eb7b0ee5b8b50d9a5e77e441b08be5a9eb903c
Gautier Michelin <gautier.michelin@gmail.com>
2020-05-13 10:00:03 +0200

    updating path

--- views/inventaire_biens_affectes_pdfgenere.php
+++ views/inventaire_biens_affectes_pdfgenere.php
@@ -4,5 +4,5 @@ $vt_registre = $this->getVar("registre");
 ?>
 <h1>Inventaire</h1>
 <p>Génération de l'inventaire PDF depuis le HTML généré...</p>
-<iframe style="width:100%;height:500px;" src="<?php print __CA_URL_ROOT__; ?>/app/plugins/museesDeFrance/render_pdf.php">
+<iframe style="width:100%;height:500px;" src="<?php print __CA_URL_ROOT__; ?>/index.php/museesDeFrance/InventaireBiensAffectes/RenderPdf">
 </iframe>
